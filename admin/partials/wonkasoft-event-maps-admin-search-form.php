4f6a33653783a2cc37ce3d13c4e946a2317ad1d3
llister15 <llister@wonkasoft.com>
2018-05-11 17:45:52 -0700

    added form

---
+++ admin/partials/wonkasoft-event-maps-admin-search-form.php
@@ -0,0 +1,18 @@
+<?php
+   /**
+ * Generate custom search form
+ *
+ * @param string $form Form HTML.
+ * @return string Modified form HTML.
+ */ 
+function wonkasoft_event_search_form( $form ) {
+    $form = '<form role="search" method="get" id="searchform" class="searchform" action="' . home_url( '/' ) . '" >
+    <div><label class="screen-reader-text" for="s">' . __( 'Search for:' ) . '</label>
+    <input type="text" value="' . get_search_query() . '" name="s" id="s" />
+    <input type="submit" id="searchsubmit" value="'. esc_attr__( 'Search' ) .'" />
+    </div>
+    </form>';
+ 
+    return $form;
+}
+add_filter( 'get_search_form', 'wonkasoft_event_search_form' );
--- includes/class-wonkasoft-event-maps.php
+++ includes/class-wonkasoft-event-maps.php
@@ -169,6 +169,7 @@ class Wonkasoft_Event_Maps {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 		$this->loader->add_action( 'admin_menu', $plugin_admin, 'wonkasoft_event_maps_add_action_links' );
 		$this->loader->add_action( 'admin_menu', $plugin_admin, 'wonkasoft_event_maps_admin_menu' );
+		$this->loader->add_action( 'wp', $plugin_admin, 'wonkasoft_event_maps_search_form' );
 
 	}
 
