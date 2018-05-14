482d60256a6beceb63c4e244f83cc7b669e20a6a
llister15 <llister@wonkasoft.com>
2018-05-14 02:23:02 -0700

    update

--- admin/partials/wonkasoft-event-maps-admin-display.php
+++ admin/partials/wonkasoft-event-maps-admin-display.php
@@ -29,21 +29,21 @@
 						<h4>Documentation for Wonkasoft Event Maps</h4>
 						<p>Shortcode Information - Instructions on how to use the Wonkasoft Event Maps plugin</p>
 						<p><strong>Default Shortcode</strong>
-						<input name="wem-shortcode-input" id="wem-shortcode-input" type="text" readonly="yes" value="[wonkasoft-event-maps]"></input><button id="copyTextbtn" onclick="copyToClipboard()"><i id="shortcode-copy" class="fas fa-copy"></i></button><span id="copyMessage" style="display: none;">Copied</span></p>
-						<form method="post" action="options.php">
-						    <?php settings_fields( 'wem_setting_group' ); ?>
-						    <?php do_settings_sections( 'wonkasoft_event_maps_admin_display' ); ?>
-						    <?php submit_button(); ?>
-						</form>
-						<h5>Here is the map that the shorcode will display</h5>
-						<?php echo do_shortcode('[wonkasoft-event-maps]'); ?>
-						<p></p>
-						<h5>Description</h5>
-						<p>Wonkasoft Event Maps was build to add a map using your event post types. You can add a map of all your post types anywhere that you uses shortcode on your site such as widgets, post, pages etc.</p>
-						<p>If you have any questions, comments, or suggestions please send all inquiries to <a href="mail:support@wonkasoft.com">support@wonkasoft.com</a>. If you would like to <a href="https://paypal.me/Wonkasoft" target="_blank">donate</a> to support us please use this <a href="https://paypal.me/Wonkasoft" target="_blank">link</a></p>
-					</div> <!-- /col-4 -->
-				</div> <!-- /row -->
-			</div> <!-- /col-8 -->
-		</div> <!-- /row -->
-	</div> <!-- /container-fluid -->
+							<input name="wem-shortcode-input" id="wem-shortcode-input" type="text" readonly="yes" value="[wonkasoft-event-maps]"></input><button id="copyTextbtn" onclick="copyToClipboard()"><i id="shortcode-copy" class="fas fa-copy"></i></button><span id="copyMessage" style="display: none;">Copied</span></p>
+							<form method="post" action="options.php">
+								<?php settings_fields( 'wem_setting_group' ); ?>
+								<?php do_settings_sections( 'wonkasoft_event_maps_admin_display' ); ?>
+								<?php submit_button(); ?>
+							</form>
+							<h5>Here is the map that the shorcode will display</h5>
+							<?php echo do_shortcode('[wonkasoft-event-maps]'); ?>
+							<p></p>
+							<h5>Description</h5>
+							<p>Wonkasoft Event Maps was build to add a map using your event post types. You can add a map of all your post types anywhere that you uses shortcode on your site such as widgets, post, pages etc.</p>
+							<p>If you have any questions, comments, or suggestions please send all inquiries to <a href="mail:support@wonkasoft.com">support@wonkasoft.com</a>. If you would like to <a href="https://paypal.me/Wonkasoft" target="_blank">donate</a> to support us please use this <a href="https://paypal.me/Wonkasoft" target="_blank">link</a></p>
+						</div> <!-- /col-4 -->
+					</div> <!-- /row -->
+				</div> <!-- /col-8 -->
+			</div> <!-- /row -->
+		</div> <!-- /container-fluid -->
 </div> <!-- /wem-settings-wrap -->
